8b035fa28062688468bcfb5602c6fa82da4f27ef
Omar Andres Plata Parra <omarplata@unisangil.edu.co>
2019-01-03 18:21:31 -0500

    Primeras configuraciones

---
+++ mao-db/index.js
@@ -0,0 +1,30 @@
+'use strict'
+
+const Sequelize = require('sequelize');
+const sequelize = new Sequelize('mao', 'root', 'root', {
+  host: '0.0.0.0',
+  dialect: 'mysql',
+
+  pool: {
+    max: 5,
+    min: 0,
+    acquire: 30000,
+    idle: 10000
+  },
+  // http://docs.sequelizejs.com/manual/tutorial/querying.html#operators
+  operatorsAliases: false
+});
+
+const User = sequelize.define('user', {
+  username: Sequelize.STRING,
+  birthday: Sequelize.DATE
+});
+
+sequelize.sync({ force: true })
+  .then(() => User.create({
+    username: 'janedoe',
+    birthday: new Date(1980, 6, 20)
+  }))
+  .then(jane => {
+    console.log(jane.toJSON());
+  });
